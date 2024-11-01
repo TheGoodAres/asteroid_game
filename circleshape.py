bc73f12a03a5927e27ae9daa6658b215133b0bf4
TheGoodAres <thegoodares@proton.me>
2024-11-01 15:45:59 +0000

    added collision detection

--- circleshape.py
+++ circleshape.py
@@ -19,3 +19,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def check_for_collision(self, circle_shape):
+        if self.position.distance_to(circle_shape.position) <= (self.radius + circle_shape.radius):
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     Player.containers= (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    asteroidField = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        
-        for object in drawable:
-            object.draw(screen)
-        screen.fill("black")
         for object in updatable:
             object.update(dt)
+        for object in asteroids:
+            if player.check_for_collision(object):
+                print("Game over!")
+                return
+            
+
+        screen.fill("black")
+        for object in drawable:
+            object.draw(screen)
+        
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
